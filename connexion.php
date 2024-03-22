c8e4354dbf58576200400c081f7f04bfc198e604
Isaacmazamesso <isaacyolou01@gmail.com>
2024-03-22 20:17:43 +0000

    init

---
+++ connexion.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="connexion_process" method="POST">
+        <label for="username"> Nom d'utilisateur</label>
+        <input type="text" id="username" name="username"> <br>
+        <label for="password">Mot de passe</label>
+        <input type="password" id="password" name="password"> <br><br>
+        
+        <input type="submit" name="submit" value="Se connecter"> 
+       
+       
+    </form>
+</body>
+</html>
+    
+    
